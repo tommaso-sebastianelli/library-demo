fb2cc3b2de2d712bdfd7013539e5d4659ef37133
Tommaso Sebastianelli <t.sebastianelli@websolute.lan>
2017-07-18 17:14:25 +0200

    created project main layout, imported angular-material modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import 'hammerjs';//md-slide-toggle, md-slider, mdTooltip
 import { NgModule } from '@angular/core';
 
+import {MdSidenavModule} from '@angular/material';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]

e7c95ae588397efbd6a86e1dd9fcf04e656f0ec3
Tommaso Sebastianelli <t.sebastianelli@websolute.lan>
2017-07-19 17:02:24 +0200

    edited sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,9 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import 'hammerjs';//md-slide-toggle, md-slider, mdTooltip
 import { NgModule } from '@angular/core';
 
+import {MdToolbarModule} from '@angular/material';
 import {MdSidenavModule} from '@angular/material';
+import {MdIconModule} from '@angular/material';
 import { AppComponent } from './app.component';
 
 
@@ -14,7 +16,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MdSidenavModule
+    MdToolbarModule,
+    MdSidenavModule,
+    MdIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]

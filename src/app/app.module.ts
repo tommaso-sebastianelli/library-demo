92829afefd18492687634ad257e7e7f63ed70043
Tommaso Sebastianelli <t.sebastianelli@websolute.lan>
2017-07-25 17:15:47 +0200

    added router to deps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import 'hammerjs';//md-slide-toggle, md-slider, mdTooltip
 import { NgModule } from '@angular/core';
 
+
+import {MdTabsModule} from '@angular/material';
+import {MdButtonModule} from '@angular/material';
 import {MdToolbarModule} from '@angular/material';
 import {MdSidenavModule} from '@angular/material';
 import {MdIconModule} from '@angular/material';
 import { AppComponent } from './app.component';
+
+//routing section
+import {RouterModule, Routes} from '@angular/router';
+const appRoutes: Routes = [
+  { path: 'crisis-center', component: null, redirectTo: '/' }
+  // { path: 'hero/:id', component: null, redirectTo: '/'},
+  // { path: 'heroes', component: null, redirectTo: '/' },
+  // { path: '',  redirectTo: '/heroes', pathMatch: 'full'},
+  // { path: '**', component: null, redirectTo: '/' }
+];
 
 
 @NgModule({
@@ -14,8 +27,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     BrowserAnimationsModule,
+    MdTabsModule,
+    MdButtonModule,
     MdToolbarModule,
     MdSidenavModule,
     MdIconModule

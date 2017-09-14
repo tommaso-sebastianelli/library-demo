7e1ed7bed76ea2ca190326f5edd9e3b50c5f0bc4
tommaso-sebastianelli <tommasosebastianelli@gmail.com>
2017-09-14 07:39:32 +0200

    search development

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import{BookService} from '../book.service';
+import {Book} from '../book';
 
 @Component({
   selector: 'app-search',
@@ -10,14 +11,31 @@ import{BookService} from '../book.service';
 })
 export class SearchComponent implements OnInit {
   title: string;
+  books: Book[];
+  activeView: number;
+
   constructor(private bookService: BookService) {
+    
   }
 
   ngOnInit() {
+    this.activeView = 0;
   }
 
-  search(keyword: string, limit: number): object{
-    return;
+  onEnter(keyword: string){
+    return this.bookService.list(keyword)
+    .then(
+      books => this.switchResults(books)    
+    );
+  }
+
+  switchResults(books:Book[]){
+    this.books = books;
+    this.activeView = 1;
+  }
+
+  switchSearch(){
+    this.activeView = 0;
   }
 
 }

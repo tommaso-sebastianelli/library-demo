b136d99582d3860af7a8ecb2215e5a72e480e3ea
tommaso-sebastianelli <tommasosebastianelli@gmail.com>
2017-09-27 23:23:23 +0200

    loan class dev

--- src/app/loans/loan.ts
+++ src/app/loans/loan.ts
@@ -1,16 +1,20 @@
 import { Book } from '../shared/book/book';
 import { LoanStatus } from './loan-status.enum';
 
+import * as moment from 'moment';
+
 export class Loan {
+    id: number;
     book: Book;
     from: string;
     to: string;
     status: LoanStatus;
 
-    constructor(book: Book, start: Date, time: number) {
-        this.from = undefined;
+    constructor(book: Book, days: number, start?: any) {
+        this.id = 0;
+        this.from = (start) ? moment(start).startOf('d').toISOString() : moment().startOf('d').toISOString();
         this.book = book;
-        this.to = undefined;
+        this.to = moment(this.from).add(days, 'd').startOf('d').toISOString();
         this.status = LoanStatus.Opened;
     }
 }

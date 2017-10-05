035a399cd0254b07608ce0654b75db4de9f6aa0d
Tommaso Sebastianelli <t.sebastianelli@websolute.lan>
2017-10-05 15:56:13 +0200

    revertible loan request - undo

--- src/app/search/loan-dialog/loan-dialog.component.ts
+++ src/app/search/loan-dialog/loan-dialog.component.ts
@@ -1,13 +1,13 @@
 import { Component, Inject, OnInit, ViewChild } from '@angular/core';
-import {MdDialogRef, MD_DIALOG_DATA} from '@angular/material';
-import {MdSnackBar} from '@angular/material';
+import { MdDialogRef, MD_DIALOG_DATA } from '@angular/material';
+import { MdSnackBar, MdSnackBarRef } from '@angular/material';
 import * as moment from 'moment';
 
 import { Observable } from 'rxjs/Observable';
 
-import {Book} from '../../shared/book/book';
-import {Loan} from '../../loans/loan';
-import {LoansService} from '../../loans/loans.service';
+import { Book } from '../../shared/book/book';
+import { Loan } from '../../loans/loan';
+import { LoansService } from '../../loans/loans.service';
 
 @Component({
   selector: 'app-loan-dialog',
@@ -15,47 +15,52 @@ import {LoansService} from '../../loans/loans.service';
   styleUrls: ['./loan-dialog.component.scss'],
 })
 export class LoanDialogComponent implements OnInit {
-public loading: boolean;
-public loanRequest: any;
-private readonly requestTimeout:number = 4000;
-@ViewChild('snackmessage') snackbar_message;
-@ViewChild('snackaction') snackbar_action;
+  public loading: boolean;
+  public loanRequest: any;
+  private timeout: any;
+  private readonly requestTimeout: number = 4000;
+  @ViewChild('snackmessage') snackbar_message;
+  @ViewChild('snackaction') snackbar_action;
   constructor(public dialogRef: MdDialogRef<LoanDialogComponent>,
-              @Inject(MD_DIALOG_DATA) public data: any,
-              public loansService:LoansService,
-              public snackBar:MdSnackBar
-) { }
+    @Inject(MD_DIALOG_DATA) public data: any,
+    public loansService: LoansService,
+    public snackBar: MdSnackBar
+  ) { }
 
   ngOnInit() {
   }
 
   loan(book: Book, weeks: number) {
-    // this.loading = true;
+    // this.loading = true;    
+    this.dialogRef.close()
     this.openSnackBar();
+    this.timeout = setTimeout(() => {
+      let model = new Loan(
+        null,
+        moment().startOf('d').toISOString(),
+        moment().add((weeks * 7) + 1, 'd').startOf('d').toISOString(),
+        book,
+        null);
 
-    let model = new Loan(
-      null,
-      moment().startOf('d').toISOString(), 
-      moment().add((weeks*7)+1, 'd').startOf('d').toISOString(), 
-      book,
-      null);
-
-    this.loanRequest = this.loansService.request(model)
-    .delay(this.requestTimeout)
-    .subscribe(
-      n => {
-        console.log(n)},
-      e => {},
-      () => {
-        this.dialogRef.close();
-      });
+      this.loansService.request(model)
+        .subscribe(
+        n => {
+          console.log(n)
+        },
+        e => { },
+        () => { });
+    }, this.requestTimeout);
   }
 
-  undo():void{
-      this.loanRequest.dispose();
+  undo(): void {
+    clearTimeout(this.timeout);
   }
 
-  openSnackBar(): void{
-    this.snackBar.open(this.snackbar_message.nativeElement.innerText, this.snackbar_action.nativeElement.innerText, {duration: this.requestTimeout});
+  openSnackBar(): void {
+    this.snackBar.open(this.snackbar_message.nativeElement.innerText, this.snackbar_action.nativeElement.innerText, { duration: this.requestTimeout })
+      .onAction()
+      .subscribe(
+      x => (this.undo())
+      );
   }
 }

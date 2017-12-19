b9fcd84a5002a74f356106e41fb96358312fb464
tommaso-sebastianelli <tommasosebastianelli@gmail.com>
2017-12-19 22:24:22 +0100

    placeholder @input setup

--- src/app/shared/placeholder/placeholder.component.ts
+++ src/app/shared/placeholder/placeholder.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-placeholder',
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./placeholder.component.scss']
 })
 export class PlaceholderComponent implements OnInit {
-
+  @Input() icon: string;
+  @Input() title: string;
+  @Input() subtitle: string;
   constructor() { }
 
   ngOnInit() {

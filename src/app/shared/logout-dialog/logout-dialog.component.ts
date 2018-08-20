e7a5ba351c926d369f7313d899c015ae25a1d1c4
Tommaso Sebastianelli <t.sebastianelli@pluservice.net>
2018-08-20 10:49:32 +0200

    redirect to search page after user logout

--- src/app/shared/logout-dialog/logout-dialog.component.ts
+++ src/app/shared/logout-dialog/logout-dialog.component.ts
@@ -3,22 +3,25 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { SocialUser } from '../../../assets/libs/angularx-social-login-master';
 import { AuthService } from '../../../assets/libs/angularx-social-login-master';
+import { Router } from '../../../../node_modules/@angular/router';
 
 @Component({
-  selector: 'app-logout-dialog',
-  templateUrl: './logout-dialog.component.html',
-  styleUrls: ['./logout-dialog.component.scss']
+	selector: 'app-logout-dialog',
+	templateUrl: './logout-dialog.component.html',
+	styleUrls: ['./logout-dialog.component.scss']
 })
 export class LogoutDialogComponent implements OnInit {
 
-  constructor(private authService: AuthService, public dialogRef: MatDialogRef<LogoutDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: SocialUser) { }
+	constructor(private authService: AuthService, public dialogRef: MatDialogRef<LogoutDialogComponent>,
+		@Inject(MAT_DIALOG_DATA) public data: SocialUser, private router: Router) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
-  private logout(a: any): void {
-    this.authService.revokeAuth().then(() => {
-      this.dialogRef.close();
-    });
-  }
+	private logout(a: any): void {
+		this.authService.revokeAuth().then(() => {
+			this.dialogRef.close();
+			this.router.navigateByUrl('/search');
+		});
+	}
 }

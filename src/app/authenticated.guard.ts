68b523e6aeddd1ba7684b67f4f536b2b1bb1ef3b
Tommaso Sebastianelli <t.sebastianelli@pluservice.net>
2018-08-20 16:44:07 +0200

    fixed auth guard misbehaviour

--- src/app/authenticated.guard.ts
+++ src/app/authenticated.guard.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { TokenService } from './shared/auth/token.service';
 
 @Injectable()
 export class AuthenticatedGuard implements CanActivate {
-	constructor(private token: TokenService) {
+	constructor(private token: TokenService, private router: Router) {
 
 	}
 
 	canActivate(
 		next: ActivatedRouteSnapshot,
 		state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-		return this.token.authClaim;
+		return this.token.authClaim.map(isAuthenticated => {
+			if (isAuthenticated) {
+				return true;
+			}
+			this.router.navigateByUrl('/search');
+			return false;
+		});
 	}
 }

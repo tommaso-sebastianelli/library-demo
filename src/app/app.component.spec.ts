0c37db470b80779a8d608d8e5bf194d6e69ca959
Tommaso Sebastianelli <t.sebastianelli@pluservice.net>
2018-08-24 12:48:21 +0200

    appComponent test fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,27 @@
 import { TestBed, async } from '@angular/core/testing';
+import { async as _async } from "rxjs/scheduler/async";
+import { of } from 'rxjs/observable/of';
 
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+import { HttpModule } from '@angular/http';
+import { ObservableMedia } from '@angular/flex-layout';
+import { MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule, MatPaginatorModule, MatMenuModule, MatDialogModule } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { appRoutes } from './app.routes';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
-
-import { RouterModule, Routes } from '@angular/router';
-import { APP_BASE_HREF } from '@angular/common';
-import { MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule } from '@angular/material';
-import { appRoutes } from './app.routes';
-import { ObservableMedia } from '@angular/flex-layout';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { PlaceholderComponent } from './shared/placeholder/placeholder.component';
+import { BookshelfComponent } from './bookshelf/bookshelf.component';
+import { GoogleSignUpButtonComponent } from './shared/google-sign-up-button/google-sign-up-button.component';
+import { VolumeShowcaseComponent } from './shared/volume-showcase/volume-showcase.component';
+import { VolumeComponent } from './shared/volume/volume.component';
+
+import { AuthService, AuthServiceConfig } from '../assets/libs/angularx-social-login-master';
+import { ApiService } from './shared/api/api.service';
+import { TokenService } from './shared/auth/token.service';
+import { BookshelvesService } from './shared/bookshelves/bookshelves.service';
 
 describe('AppComponent', () => {
 	beforeEach(async(() => {
@@ -22,19 +34,38 @@ describe('AppComponent', () => {
 				MatIconModule,
 				MatButtonModule,
 				MatSidenavModule,
-				NoopAnimationsModule
+				MatPaginatorModule,
+				MatDialogModule,
+				NoopAnimationsModule,
+				MatMenuModule,
+				HttpModule
 			],
 			declarations: [
 				AppComponent,
 				SearchComponent,
-				PlaceholderComponent
+				PlaceholderComponent,
+				BookshelfComponent,
+				GoogleSignUpButtonComponent,
+				VolumeShowcaseComponent,
+				VolumeComponent
 			],
 			providers: [
 				{ provide: APP_BASE_HREF, useValue: '/' },
+				{ provide: AuthService, useValue: AuthServiceConfig },
+				{ provide: TokenService, useValue: TokenServiceStub },
+				BookshelvesService,
+				ApiService,
 				ObservableMedia
 			]
 		}).compileComponents();
 	}));
+
+	const TokenServiceStub = {
+		get authClaim() {
+			const isAuthenticated: boolean = false;
+			return of(isAuthenticated, _async);
+		}
+	};
 
 	it('should create the app', async(() => {
 		const fixture = TestBed.createComponent(AppComponent);

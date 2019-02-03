d353b29d8e048f4c954f6408483070a9f963b17b
tommaso-sebastianelli <tommasosebastianelli@gmail.com>
2019-02-03 13:49:44 +0100

    Auto-generated commit

---
+++ assets/libs/angularx-social-login-master/src/entities/login-provider.ts
@@ -0,0 +1,9 @@
+import { SocialUser } from './user';
+
+export interface LoginProvider {
+	initialize(): Promise<SocialUser>;
+	signIn(): Promise<SocialUser>;
+	signOut(): Promise<any>;
+	revokeAuth(): Promise<any>;
+}
+
